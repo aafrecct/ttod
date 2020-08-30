761721ad0106e84c618cdb5a2da5d02896906c91
Borja Martinena <aafrected@gmail.com>
2020-08-30 13:38:54 +0200

    Clean attempt 1

--- tmp.py
+++
@@ -1,9 +0,0 @@
-class Class():
-    def __init__(self):
-        self.dict = {'key': self.f}
-    def f(self, s):
-        return s + ' procesed.'
-
-c = Class()
-for v in c.dict:
-    print(c.dict[v](v))

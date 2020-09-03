c64156401f78acd57f468ef84a67105b671734d3
aafrecct <aafrected@gmail.com>
2020-09-03 20:07:12 +0200

    Miedo y respeto y todo lo demás

--- game/tod_game.py
+++ game/tod_game.py
@@ -1,22 +1,17 @@
 from random import randint, choice, shuffle
+from yaml import safe_load
 
 def read_questions(path):
-    '''Returns a dictionary with the grouped questions in the given file.
+    '''Returns a dictionary with the grouped questions in the given folder.
+    Looks for files "truths.yaml" and "dares.yaml" inside the folder.
 
     Keyword arguments:
     path -- The path to the file containing the questions.
     '''
-    questions = {}
-    with open(path, 'r') as questionsfile:
-        last_header = ""
-        for line in questionsfile.readlines():
-            l = line[:-1]
-            if l[0] == '[':
-                last_header = l[1:-1].lower()
-                questions[last_header] = []
-            else:
-                questions[last_header].append((int(l[:5]), l[5:]))
-
+    questions = {'truths': {}, 'dares': {}}
+    for key in questions:
+        with open(path + "/{}.yaml".format(key), 'r') as questionsfile:
+            questions[key] = safe_load(questionsfile)
     return questions
 
 
@@ -25,16 +20,15 @@ class TODGame():
 
     Attributes:
     players        -- A list of objects of the Player class.
-    truths         -- A dictionary from the lines of the file 'truths'.
-    dares          -- A dictionary from the lines of the file 'dares'.
+    truths         -- A dictionary from the lines of the file 'truths.yaml'.
+    dares          -- A dictionary from the lines of the file 'dares.yaml'.
     round_counter  -- A counter of the number of rounds since the beginning.
     player_counter -- A counter indicating the player's whose turn it is.
     '''
     
     def __init__(self, players, language='en'):
         self.players = players
-        self.truths = read_questions('translations/{0}/truths'.format(language))
-        self.dares = read_questions('translations/{0}/dares'.format(language))
+        self.questions = read_questions('translations/{0}'.format(language))
         self.round_counter = 0
         self.player_counter = -1
 
@@ -45,26 +39,29 @@ class TODGame():
         return choice([p for p in self.players if p.name != player.name])
 
     def is_truth(self, question):
-        return question in self.truths['all'] + self.truths['single'] + self.truths['couples']  
+        b = False
+        for lst in self.questions['truths'].values():
+            b = b or question in lst
+        return b
 
-    def _player_question_pool(self, kind, player):
+    def player_question_pool(self, kind, player):
         if kind == 'truth':
-            pool_d = self.truths
+            pool_d = self.questions['truths']
         elif kind == 'dare':
-            pool_d = self.dares
+            pool_d = self.questions['dares']
         else:
             pool_d = {}
-            for key in self.truths.keys():
+            for key in self.questions['truths'].keys():
                 pool_d[key] = self.truths[key] + self.dares[key]
         pool = pool_d['all']
-        pool += pool_d['single'] if player.partner == None else pool_d['couples']
+        pool += pool_d['single'] if player.partner is None else pool_d['couples']
         return pool
 
-    def _random_norepeat_question(self, kind, player):
-        q_pool = self._player_question_pool(kind, player)
-        used_of_the_kind = {'truth':player.used_truths,
-                            'dare':player.used_dares,
-                            'both':player.used_questions}
+    def _random_norep_question(self, kind, player):
+        q_pool = self.player_question_pool(kind, player)
+        used_of_the_kind = {'truth': player.used_truths,
+                            'dare': player.used_dares,
+                            'both': player.used_questions}
         max_questions = len(q_pool) - len(used_of_the_kind[kind])
         question = q_pool[randint(0, max_questions - 1)]
         if player.is_used(question[0]):
@@ -85,6 +82,5 @@ class TODGame():
 
     def ask_question(self, kind):
         player = self.players[self.player_counter]
-        question = self._question_tuple(player, self._random_norepeat_question(kind, player))
+        question = self._question_tuple(player, self._random_norep_question(kind, player))
         return question
-
